e9ae14aa2c13f5649cefbd393f7d1be7d2cb3e20
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-05-22 00:54:16 -0300

    comeco navbar

---
+++ portfolio/app/components/NavLink.jsx
@@ -0,0 +1,3 @@
+const NavLink = ({ href, title }) => {
+    
+}
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,12 +1,13 @@
-import { Container } from "postcss";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-    <div class="container mx-auto px-12 py-4"> {/*para a tela ficar preenchida ate o final, sem as bordas cortadas*/}
-      <HeroSection/>
-    </div>
+      <Navbar />
+     <div class="container mt-24 mx-auto px-12 py-4"> {/*para a tela ficar preenchida ate o final, sem as bordas cortadas*/}
+       <HeroSection/>
+     </div>
     </main>
-  );
+  )
 }
