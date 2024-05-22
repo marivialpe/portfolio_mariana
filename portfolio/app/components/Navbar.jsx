38c34d6e9073d3a14ef03e55ab1ff5d8e89967cc
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-05-22 16:15:49 -0300

    29_33

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -10,7 +10,14 @@ const Navbar = () => {
              </Link>
              <div className="menu hidden md:block md:w-auto" id="navbar">
                 <ul>
-                    <li><Link href={'#about'} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">Sobre</Link></li>
+                    <li>
+                        <Link 
+                    href={'#about'} 
+                    className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+                >
+                   Sobre
+                        </Link>
+                    </li>
                 </ul>
              </div>
             </div>
