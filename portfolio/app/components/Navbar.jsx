5c7f2a648e5398cdddd70e5918c3df9151aa5436
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-07-03 00:13:20 -0300

    a

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,5 +1,21 @@
 import Link from "next/link";
 import React from "react";
+import NavLink from "./NavLink";
+
+const NavLinks = [
+    {
+        title: "Sobre",
+        path: "#sobre",
+    },
+    {
+        title: "Projetos",
+        path: "#projetos",
+    },
+    {
+        title: "Contato",
+        path: "#contato",
+    }
+]
 
 const Navbar = () => {
     return (
@@ -10,14 +26,14 @@ const Navbar = () => {
              </Link>
              <div className="menu hidden md:block md:w-auto" id="navbar">
                 <ul>
-                    <li>
-                        <Link 
-                    href={'#about'} 
-                    className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-                >
-                   Sobre
-                        </Link>
-                    </li>
+                    {
+                        NavLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink href={link.path} title={link.title} />
+                            </li>
+                         )
+                        )
+                    }
                 </ul>
              </div>
             </div>
