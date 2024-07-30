1681021b4b6b938058b6fb79d9fbf742dedbef95
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-07-30 14:56:57 -0300

    ➕navbar(36:44)

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,6 +1,8 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import NavLink from "./NavLink";
+import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
 
 const NavLinks = [
     {
@@ -18,14 +20,24 @@ const NavLinks = [
 ]
 
 const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-        <nav>
-            <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-             <Link href={'/'} className="text-5xl text-white font-semibold">
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+            <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+             <Link href={'/'} className="text-2xl md:text-5xl text-white font-semibold">
                 LOGO
              </Link>
+             <div className="mobile-menu block md:hidden">
+                {
+                    navbarOpen ? (
+                        <button></button>
+                    ) : (
+                        <button></button>
+                    )
+                }
+                </div>
              <div className="menu hidden md:block md:w-auto" id="navbar">
-                <ul>
+                <ul className="flex p-4 md:p-0 sm:flex-row md:space-x-8 mt-0">
                     {
                         NavLinks.map((link, index) => (
                             <li key={index}>
